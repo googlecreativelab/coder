693385e5cc1c63905047e831afa65fe87505c7e6
Jason Striegel <jason.striegel@gmail.com>
2013-12-03 19:57:11 -0500

    gpio/sockets test support

--- coder-apps/tests/socket_test/static/js/index.js
+++ coder-apps/tests/socket_test/static/js/index.js
@@ -3,14 +3,24 @@ $(document).ready( function() {
 
     //This code will run after your page loads
     Coder.socketConnection.init(function(){
-        console.log("connected with ID: " + Coder.socketConnection.socketID);
         
-        Coder.socketConnection.sendData( 'connect', {'name':'jimmy'} );
+        addOutputMessage( "Connected with ID: " + Coder.socketConnection.socketID );
+        
+        
+        Coder.socketConnection.sendData( 'connect', {'name':'testing'} );
 
         Coder.socketConnection.addListener( 'message', function( d ){
             console.log("message from: " + d.name + " : " + d.message);
         });
     });
 
+    addOutputMessage( "Connecting... see the debug console for log messages." );
+
 });
+
+var addOutputMessage = function( text ) {
+    var $output = $("#output");
+    $output.prepend( $("<p/>").text( text ) );
+    console.log( text );
+}
 
